9b0e53516cefba7eb88ad0a1cf34db42e0cddc77
michael nowlin <michaelnowlin5@gmail.com>
2018-04-13 17:46:56 +0000

    update

--- js/smn.js
+++ js/smn.js
@@ -1,19 +1,278 @@
-function moveRed(){
-       if(main_x2 >= 0 && speed2 > 0){
+// variables that could be usefull
+// fight global
+var q = 0;
+var f = 0;
+var c = 0;
+var z = 0;
+var little = false;
+var game; //canvas
+var battle; //background
+var map; // background
+var context;
+var player1;
+var player2;
+
+var speed = 0;
+var horizontal = 0;
+var speed2 = 0;
+var horizontal2 = 0;
+var PlayerDmg = Math.floor(Math.random() * 6); // damage that player outputs
+var battle_music = document.getElementById("battle_music");
+
+var main_x = document.getElementById("blueFight"), main_y = document.getElementById("blueFight"), move_x = 0 , move_y = 0;
+var main_x2 =  document.getElementById("redFight"), main_y2 =  document.getElementById("redFight"), move_x2 = 0, move_y2 = 0;
+
+var hammerplay = document.getElementById("blueFight");
+var swordplay = document.getElementById("redFight");
+var framerate = 25;
+var map_music = document.getElementById("map_music");
+
+
+   
+function keyPressHammer(e){
+   
+    if(little === true){
+    
+           if(e.keyCode == 68){
+      
+      console.log(hammerplay);
+      if (hammerplay == null){
+          hammerplay = document.getElementById("hammerplay");
+      }
+        if (main_x == null){
+          main_x = document.getElementById("hammerplay");
+      }
+         speed = 1.5;
+        move_x = speed;
+      
+      document.getElementById("hammerplay").style.transform = "rotate(90deg)";
+           }
+    
+    if(e.keyCode == 65){
+        speed = -1.5;
+    move_x = speed;
+    document.getElementById("hammerplay").style.transform = "rotate(-90deg)";
+    }
+    
+    if(e.keyCode == 83){
+          if (main_y == null){
+          main_y = document.getElementById("hammerplay");
+      }
+        horizontal = 1.5;
+    move_y = horizontal;
+   
+    document.getElementById("hammerplay").style.transform = "rotate(180deg)";
+    }
+    if(e.keyCode == 87){
+        horizontal = -1.5;
+    move_y = horizontal;
+    document.getElementById("hammerplay").style.transform = "rotate(360deg)";
+    }
+     if(e.keyCode == 32) {//attack?
+         
+     }
+      if(e.keyCode == 66) { // attack?
+       
+     }
+    moveHammer();
+    }
+}
+
+function keyReleaseHammer(e){
+    
+    if(e.keyCode == 68){
+        move_x = 0;
+      //  document.getElementById("blueFight").style.transform = "rotate(90deg)";
+    }
+      if(e.keyCode == 65){
+        move_x = 0;
+        
+    }
+      if(e.keyCode == 83){
+        move_y = 0;
+    
+       //  document.getElementById("blueFight").style.transform = "rotate(180deg)";
+    }
+      if(e.keyCode == 87){
+        move_y = 0;
+         
+        // document.getElementById("blueFight").style.transform = "rotate(360deg)";
+    }
+     if(e.keyCode == 32) {
+       //  document.getElementsByClassName("blueFight").style.backgroundImage = "url('images/BlueBoardSolo.png')"
+     }
+ 
+}
+function keyPressSword(e){
+   if(little === true){
+       if(e.keyCode == 39){
+           
+              if (swordplay == null){
+          swordplay = document.getElementById("swordplay");
+      }
+        if (main_x2 == null){
+          main_x2 = document.getElementById("swordplay");
+      }
+           
+           speed2 = 1.5;
+        move_x2 = speed2;
+        
+        document.getElementById("swordplay").style.transform = "rotate(90deg)";
+    }
+    if(e.keyCode == 37){
+        speed2 = -1.5;
+    move_x2 = speed2;
+        
+         document.getElementById("swordplay").style.transform = "rotate(-90deg)";
+    }
+    if(e.keyCode == 40){
+          if (main_y2 == null){
+          main_y2 = document.getElementById("swordplay");
+      }
+        horizontal2 = 1.5;
+    move_y2 = horizontal2;
+        
+         document.getElementById("swordplay").style.transform = "rotate(180deg)";
+    }
+    if(e.keyCode == 38){
+        horizontal2 = -1.5;
+    move_y2 = horizontal2;
+        document.getElementById("swordplay").style.transform = "rotate(360deg)";
+    }
+   moveSword();
+   }
+}
+function keyReleaseSword(e){
+    if(e.keyCode == 39){
+        move_x2 = 0;
+      //  document.getElementById("redFight").style.transform = "rotate(90deg)";
+    }
+      if(e.keyCode == 37){
+        move_x2 = 0;
+     //   document.getElementById("redFight").style.transform = "rotate(-90deg)";
+    }
+      if(e.keyCode == 40){
+        move_y2 = 0;
+      //  document.getElementById("redFight").style.transform = "rotate(180deg)";
+    }
+      if(e.keyCode == 38){
+        move_y2 = 0;
+      //  document.getElementById("redFight").style.transform = "rotate(360deg)";
+    }
+   
+}
+
+function moveHammer(){
+   c += move_x;
+     z += move_y;
+      var res = 1175;
+  var x = res + c;
+   var str = 600;
+   var y = str + z;
+         
+          if(main_x >= 10 && speed > 0){
+            main_x += 0;
+        }
+          else if(main_x <= -9 && speed < 0){
+         main_x += 0;
+     }else{
+      document.getElementById("hammerplay").style.left = (x).toString() + "px";
+           
+            
+           
+     
+      }
+         if(main_y >= 10 && horizontal > 0){
+        main_y += 0;
+      }
+      else  if (main_y <= -9 && horizontal < 0){
+        main_y += 0;
+        }else{
+        
+             document.getElementById("hammerplay").style.top = (y).toString() + "px";
+             
+         }
+    
+   
+}
+function moveSword(){
+      q += move_x2;
+      f += move_y2;
+       var zez = 500;
+  var x2 = zez + q;
+  
+  
+   var sst = 100;
+   var y2 = sst + f;
+     
+       if(main_x2 >= 10 && speed2 > 0){
             main_x2 += 0;
         }
-          else if(main_x2 <= 1000 && speed2 < 0){
+          else if(main_x2 <= -9 && speed2 < 0){
          main_x2 += 0;
      }else{
-         main_x2 += move_x2;
+         document.getElementById("swordplay").style.left = (x2).toString() + "px";
      }
-       
-      if(main_y2 >= 0 && horizontal2 > 0){
+     if(main_y2 >= 10 && horizontal2 > 0){
         main_y2 += 0;
       }
-      else  if (main_y2 <= 750 && horizontal2 < 0){
+      else  if (main_y2 <= -9 && horizontal2 < 0){
         main_y2 += 0;
         }else{
-            main_y2 += move_y2;
+            document.getElementById("swordplay").style.top = (y2).toString() + "px";
         }
-};
+   
+}
+
+
+function draw(){
+       context.save();
+      context.clearRect(0,0,game.width,game.height);
+      
+     context.drawImage(hammerplay, game.width/2 + main_x, game.height/2 + main_y );
+       context.drawImage(swordplay, game.width/2 + main_x, game.height/2 + main_y );
+       
+          context.restore();
+  
+ }
+
+function gameloop(){
+    
+  //  draw();
+  if(little === false){
+       
+       
+  }
+  else if(little ===true){
+      moveHammer();
+    moveSword();
+  }
+  
+  
+}
+
+
+
+function init() {
+     var but = document.getElementsByClassName("battle");
+     game = document.getElementById("land");
+     if (game && game.getContext) {
+    context = game.getContext('2d');
+    setInterval(this.gameLoop,1000/100);
+    window.canvas = document.getElementById("space");
+    window.ctx_1 = game.getContext("2d");
+     window.addEventListener("keydown", keyPressSword, false);
+     window.addEventListener("keyup", keyReleaseSword, false);
+     window.addEventListener("keydown", keyPressHammer, false);
+     window.addEventListener("keyup", keyReleaseHammer, false); 
+     }
+     
+     but.onclick = function(){
+         little = true;
+     }
+}
+
+  
+
+
+window.addEventListener("load", this.init, false);
